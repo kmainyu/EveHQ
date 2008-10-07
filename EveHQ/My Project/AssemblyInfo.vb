04740997b3ad88db28a52fd85caa82ea2e8d4dea
vessper <none@none>
2008-10-07 23:34:34 +0000

    07/10/2008 (1.7.5.137) ---------- - Updated version numbers - Released!

--- EveHQ/My Project/AssemblyInfo.vb
+++ EveHQ/My Project/AssemblyInfo.vb
@@ -52,5 +52,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.7.5.135")> 
-<Assembly: AssemblyFileVersion("1.7.5.135")> 
+<Assembly: AssemblyVersion("1.7.5.137")> 
+<Assembly: AssemblyFileVersion("1.7.5.137")> 
