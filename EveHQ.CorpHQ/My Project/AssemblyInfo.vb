21ffc07d23a02d2997df27578c5fea815c321c39
vessper <none@none>
2009-03-22 01:59:47 +0000

    21/03/2009 (1.9.0.456) ---------- - Core: Removed XML docs from project compilation - Core: Updated version numbers, XML and cache versions

--- EveHQ.CorpHQ/My Project/AssemblyInfo.vb
+++ EveHQ.CorpHQ/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.8.5.420")> 
-<Assembly: AssemblyFileVersion("1.8.5.420")> 
+<Assembly: AssemblyVersion("1.9.0.456")> 
+<Assembly: AssemblyFileVersion("1.9.0.456")> 
