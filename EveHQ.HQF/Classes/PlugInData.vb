50df9b60d260eb59667957dcc80a47122ee512eb
vessper <none@none>
2009-04-01 20:08:26 +0000

    01/04/2009 (1.9.3.493) ---------- - HQF: Fixed mapping of T2 missile damage from Heavy Assault Missile Skills - HQF: Updated cache version - HQF: Updated version & XML

--- EveHQ.HQF/Classes/PlugInData.vb
+++ EveHQ.HQF/Classes/PlugInData.vb
@@ -12,7 +12,7 @@ Public Class PlugInData
     Shared moduleEffectData As DataSet
     Shared moduleAttributeData As DataSet
     Shared UseSerializableData As Boolean = False
-    Shared LastCacheRefresh As String = "1.9.3.492"
+    Shared LastCacheRefresh As String = "1.9.3.493"
 
 #Region "Plug-in Interface Properties and Functions"
 
