90e3d1a0e9704453711619c19881b8227a069957
vessper <none@none>
2009-04-07 13:06:07 +0000

    07/04/2009 (1.9.3.503) ---------- - Core: Removed static image from About Form

--- EveHQ/Forms/frmAbout.Designer.vb
+++ EveHQ/Forms/frmAbout.Designer.vb
@@ -19,19 +19,18 @@ Partial Class frmAbout
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAbout))
         Me.Panel1 = New System.Windows.Forms.Panel
-        Me.lblEveHQLink = New System.Windows.Forms.LinkLabel
-        Me.wbCredits = New System.Windows.Forms.WebBrowser
         Me.lblCopyright = New System.Windows.Forms.Label
         Me.lblDate = New System.Windows.Forms.Label
         Me.lblVersion = New System.Windows.Forms.Label
+        Me.lblEveHQLink = New System.Windows.Forms.LinkLabel
+        Me.wbCredits = New System.Windows.Forms.WebBrowser
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
         '
-        Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
+        Me.Panel1.BackColor = System.Drawing.Color.Black
         Me.Panel1.Controls.Add(Me.lblCopyright)
         Me.Panel1.Controls.Add(Me.lblDate)
         Me.Panel1.Controls.Add(Me.lblVersion)
@@ -40,37 +39,6 @@ Partial Class frmAbout
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(600, 400)
         Me.Panel1.TabIndex = 0
-        '
-        'lblEveHQLink
-        '
-        Me.lblEveHQLink.ActiveLinkColor = System.Drawing.Color.DarkTurquoise
-        Me.lblEveHQLink.AutoSize = True
-        Me.lblEveHQLink.BackColor = System.Drawing.Color.Transparent
-        Me.lblEveHQLink.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblEveHQLink.ForeColor = System.Drawing.Color.PaleTurquoise
-        Me.lblEveHQLink.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
-        Me.lblEveHQLink.LinkColor = System.Drawing.Color.PaleTurquoise
-        Me.lblEveHQLink.Location = New System.Drawing.Point(12, 365)
-        Me.lblEveHQLink.Name = "lblEveHQLink"
-        Me.lblEveHQLink.Size = New System.Drawing.Size(336, 13)
-        Me.lblEveHQLink.TabIndex = 4
-        Me.lblEveHQLink.TabStop = True
-        Me.lblEveHQLink.Text = "Visit the forums at www.evehq.net for bug reporting and comments."
-        Me.lblEveHQLink.VisitedLinkColor = System.Drawing.Color.PaleTurquoise
-        '
-        'wbCredits
-        '
-        Me.wbCredits.AllowNavigation = False
-        Me.wbCredits.AllowWebBrowserDrop = False
-        Me.wbCredits.Dock = System.Windows.Forms.DockStyle.Right
-        Me.wbCredits.IsWebBrowserContextMenuEnabled = False
-        Me.wbCredits.Location = New System.Drawing.Point(603, 0)
-        Me.wbCredits.MinimumSize = New System.Drawing.Size(20, 20)
-        Me.wbCredits.Name = "wbCredits"
-        Me.wbCredits.ScriptErrorsSuppressed = True
-        Me.wbCredits.ScrollBarsEnabled = False
-        Me.wbCredits.Size = New System.Drawing.Size(196, 399)
-        Me.wbCredits.TabIndex = 1
         '
         'lblCopyright
         '
@@ -110,6 +78,37 @@ Partial Class frmAbout
         Me.lblVersion.TabIndex = 5
         Me.lblVersion.Text = "Version"
         Me.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'lblEveHQLink
+        '
+        Me.lblEveHQLink.ActiveLinkColor = System.Drawing.Color.DarkTurquoise
+        Me.lblEveHQLink.AutoSize = True
+        Me.lblEveHQLink.BackColor = System.Drawing.Color.Transparent
+        Me.lblEveHQLink.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEveHQLink.ForeColor = System.Drawing.Color.PaleTurquoise
+        Me.lblEveHQLink.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
+        Me.lblEveHQLink.LinkColor = System.Drawing.Color.PaleTurquoise
+        Me.lblEveHQLink.Location = New System.Drawing.Point(12, 365)
+        Me.lblEveHQLink.Name = "lblEveHQLink"
+        Me.lblEveHQLink.Size = New System.Drawing.Size(336, 13)
+        Me.lblEveHQLink.TabIndex = 4
+        Me.lblEveHQLink.TabStop = True
+        Me.lblEveHQLink.Text = "Visit the forums at www.evehq.net for bug reporting and comments."
+        Me.lblEveHQLink.VisitedLinkColor = System.Drawing.Color.PaleTurquoise
+        '
+        'wbCredits
+        '
+        Me.wbCredits.AllowNavigation = False
+        Me.wbCredits.AllowWebBrowserDrop = False
+        Me.wbCredits.Dock = System.Windows.Forms.DockStyle.Right
+        Me.wbCredits.IsWebBrowserContextMenuEnabled = False
+        Me.wbCredits.Location = New System.Drawing.Point(603, 0)
+        Me.wbCredits.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.wbCredits.Name = "wbCredits"
+        Me.wbCredits.ScriptErrorsSuppressed = True
+        Me.wbCredits.ScrollBarsEnabled = False
+        Me.wbCredits.Size = New System.Drawing.Size(196, 399)
+        Me.wbCredits.TabIndex = 1
         '
         'frmAbout
         '
