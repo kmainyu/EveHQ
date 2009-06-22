99e8a49a4b77838d328787af366d84d46e393ec3
vessper <none@none>
2009-06-22 22:16:55 +0000

    22/06/2009 (1.11.3.637) ---------- - Prism: Updated build number to 637 for update release

--- EveHQ.Prism/My Project/AssemblyInfo.vb
+++ EveHQ.Prism/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.11.3.616")> 
-<Assembly: AssemblyFileVersion("1.11.3.616")> 
+<Assembly: AssemblyVersion("1.11.3.637")> 
+<Assembly: AssemblyFileVersion("1.11.3.637")> 
