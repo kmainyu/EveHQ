735bfa7a572f6a492cc4aa3e02885780ea0a41c0
vessper <none@none>
2009-07-11 21:17:25 +0000

    11/07/2009 (1.12.0.721) ---------- - Prism: Updated version numbers for testing - POS: Updated version numbers for testing

--- EveHQ.Prism/My Project/AssemblyInfo.vb
+++ EveHQ.Prism/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.12.0.714")> 
-<Assembly: AssemblyFileVersion("1.12.0.714")> 
+<Assembly: AssemblyVersion("1.12.0.721")> 
+<Assembly: AssemblyFileVersion("1.12.0.721")> 
