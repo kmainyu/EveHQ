1dde40b84e3df14d9d4ae179baf7515480636a2f
vessper <none@none>
2009-08-18 21:53:07 +0000

    18/08/2009 (1.12.1.798) ---------- - Core: Updated HQF Cache version - Core: Updated version history, versions and update files

--- EveHQ.HQF/Classes/PlugInData.vb
+++ EveHQ.HQF/Classes/PlugInData.vb
@@ -12,7 +12,7 @@ Public Class PlugInData
     Shared moduleEffectData As DataSet
     Shared moduleAttributeData As DataSet
     Shared UseSerializableData As Boolean = False
-    Shared LastCacheRefresh As String = "1.12.1.770"
+    Shared LastCacheRefresh As String = "1.13.0.798"
 
 #Region "Plug-in Interface Properties and Functions"
 
--- EveHQ.Prism/My Project/AssemblyInfo.vb
+++ EveHQ.Prism/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.12.1.770")> 
-<Assembly: AssemblyFileVersion("1.12.1.770")> 
+<Assembly: AssemblyVersion("1.13.0.798")> 
+<Assembly: AssemblyFileVersion("1.13.0.798")> 
