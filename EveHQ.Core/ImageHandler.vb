c684f4ecc97c3ac8df513e99a3e59ce409f4884a
vessper <none@none>
2009-08-30 10:35:50 +0000

    30/08/2009 (1.13.1.832) ---------- - Core: Added routine to ImageHandler to return an image

--- EveHQ.Core/ImageHandler.vb
+++ EveHQ.Core/ImageHandler.vb
@@ -28,7 +28,29 @@ Public Class ImageHandler
 
     End Function
 
-    Private Shared Sub DownloadImage(ByVal DownloadInfoObject As Object)
+    Public Shared Function GetImage(ByVal imageName As String, ByVal imageType As Integer) As Image
+        ' Check EveHQ image cache folder for the image
+        If My.Computer.FileSystem.FileExists(Path.Combine(EveHQ.Core.HQ.imageCacheFolder, imageName & ".png")) = True Then
+            ' Return the cached image location
+            Return Image.FromFile(Path.Combine(EveHQ.Core.HQ.imageCacheFolder, imageName & ".png"))
+        Else
+            ' Calculate the remote URL
+            Dim remoteURL As String = ""
+            Select Case imageType
+                Case ImageHandler.ImageType.Blueprints
+                    remoteURL = "http://www.evehq.net/eve/images/blueprints/64_64/" & imageName & ".png"
+                Case ImageHandler.ImageType.Icons
+                    remoteURL = "http://www.evehq.net/eve/images/icons/64_64/icon" & imageName & ".png"
+                Case ImageHandler.ImageType.Types
+                    remoteURL = "http://www.evehq.net/eve/images/types/128_128/" & imageName & ".png"
+            End Select
+            ' Download the image
+            Return DownloadImage(New Object() {remoteURL, imageName})
+        End If
+
+    End Function
+
+    Private Shared Function DownloadImage(ByVal DownloadInfoObject As Object) As Image
         Try
             Dim DownloadInfo() As Object = CType(DownloadInfoObject, Object())
             Dim remoteURL As String = CStr(DownloadInfo(0))
@@ -38,11 +60,13 @@ Public Class ImageHandler
             Dim WebImage As Image = Image.FromStream(ResponsePic.GetResponseStream())
             ' Save the image into the cache
             WebImage.Save(Path.Combine(EveHQ.Core.HQ.imageCacheFolder, imageName & ".png"), System.Drawing.Imaging.ImageFormat.Png)
+            Return WebImage
         Catch ex As Exception
             ' Assume this is because the image was not found - which happens for a lot of the database items
             ' No need to do anything with the error, just carry on
+            Return Nothing
         End Try
-    End Sub
+    End Function
 
     Public Enum ImageType As Integer
         Blueprints = 0
