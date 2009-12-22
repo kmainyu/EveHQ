35416b98f6c3014af22ad12adf6b4d176a84a4d4
vessper <none@none>
2009-12-22 10:56:43 +0000

    22/12/2009 ---------- - Core: Added new EveMail class to the Core.dll

---
+++ EveHQ.Core/EveMail.vb
@@ -0,0 +1,16 @@
+﻿Public Class EveMail
+
+
+
+End Class
+
+Public Class EveMailMessage
+    Public MessageID As Long
+    Public SenderID As Long
+    Public MessageDate As Date
+    Public MessageTitle As String
+    Public ToCorpAllianceIDs As String
+    Public ToCharacterIDs As String
+    Public ToListIDs As String
+    Public ReadFlag As Boolean
+End Class
