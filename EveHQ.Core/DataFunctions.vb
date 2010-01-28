f49cb0a1707d93dd7a485eba6aa69a5822134a43
vessper <none@none>
2010-01-28 15:47:44 +0000

    28/01/2010 ---------- - Core: Added function to write MailingList IDs to ID database

--- EveHQ.Core/DataFunctions.vb
+++ EveHQ.Core/DataFunctions.vb
@@ -1666,4 +1666,39 @@ Public Class DataFunctions
         Next
     End Sub
 
+    Public Shared Sub WriteMailingListIDsToDatabase(ByVal mPilot As EveHQ.Core.Pilot)
+        Dim accountName As String = mPilot.Account
+        Dim mAccount As EveHQ.Core.EveAccount = CType(EveHQ.Core.HQ.EveHQSettings.Accounts.Item(accountName), Core.EveAccount)
+        ' Send this to the API
+        Dim IDXML As XmlDocument = EveHQ.Core.EveAPI.GetAPIXML(EveHQ.Core.EveAPI.APIRequest.MailingLists, mAccount, mPilot.ID, EveHQ.Core.EveAPI.APIReturnMethod.ReturnStandard)
+        ' Parse this XML
+        Dim FinalIDs As New SortedList(Of Long, String)
+        Dim IDList As XmlNodeList
+        Dim IDNode As XmlNode
+        Dim eveID As Long = 0
+        Dim eveName As String = ""
+        IDList = IDXML.SelectNodes("/eveapi/result/rowset/row")
+        If IDList.Count > 0 Then
+            For Each IDNode In IDList
+                eveID = CLng(IDNode.Attributes.GetNamedItem("listID").Value)
+                eveName = IDNode.Attributes.GetNamedItem("displayName").Value
+                If FinalIDs.ContainsKey(eveID) = False Then
+                    FinalIDs.Add(eveID, eveName)
+                End If
+            Next
+        End If
+        ' Add all the data to the database
+        Dim strIDInsert As String = "INSERT INTO eveIDToName (eveID, eveName) VALUES "
+        For Each eveID In FinalIDs.Keys
+            eveName = FinalIDs(eveID)
+            Dim uSQL As New StringBuilder
+            uSQL.Append(strIDInsert)
+            uSQL.Append("(" & eveID & ", ")
+            uSQL.Append("'" & eveName & "');")
+            If EveHQ.Core.DataFunctions.SetData(uSQL.ToString) = False Then
+                'MessageBox.Show("There was an error writing data to the Eve ID database table. The error was: " & ControlChars.CrLf & ControlChars.CrLf & EveHQ.Core.HQ.dataError & ControlChars.CrLf & ControlChars.CrLf & "Data: " & uSQL.ToString, "Error Writing Eve IDs", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            End If
+        Next
+    End Sub
+
 End Class
