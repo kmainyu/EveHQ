499c0b6992c7d973e5b30c8b413855d6e73965cb
vessper <none@none>
2010-02-02 22:39:13 +0000

    02/02/2010 (1.14.2.1290) ---------- - Core: Updated version history - Core: Updated version numbers - Core: Updated cache file numbers - Core: Updated XML files

--- EveHQ.Core/My Project/AssemblyInfo.vb
+++ EveHQ.Core/My Project/AssemblyInfo.vb
@@ -50,6 +50,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.14.1.1283")> 
-<Assembly: AssemblyFileVersion("1.14.1.1283")> 
+<Assembly: AssemblyVersion("1.14.2.1290")> 
+<Assembly: AssemblyFileVersion("1.14.2.1290")> 
 
--- EveHQ.Prism/My Project/AssemblyInfo.vb
+++ EveHQ.Prism/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.14.1.1287")> 
-<Assembly: AssemblyFileVersion("1.14.1.1287")> 
+<Assembly: AssemblyVersion("1.14.2.1290")> 
+<Assembly: AssemblyFileVersion("1.14.2.1290")> 
