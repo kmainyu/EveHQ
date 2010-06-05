db4623363e002ce03825607ef90329d35a7ce363
vessper <none@none>
2010-06-05 08:03:41 +0000

    Core: Update trunk build to 1820 for update

--- EveHQ.PosManager/Properties/AssemblyInfo.cs
+++ EveHQ.PosManager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EveHQ.PostManager")]
+[assembly: AssemblyTitle("EveHQ.PosManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EveHQ.PostManager")]
+[assembly: AssemblyProduct("EveHQ.PosManager")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.15.2.1810")]
-[assembly: AssemblyFileVersion("1.15.2.1810")]
+[assembly: AssemblyVersion("1.15.2.1820")]
+[assembly: AssemblyFileVersion("1.15.2.1820")]
