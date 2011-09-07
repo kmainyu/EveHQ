f1d1108ab54f74fac6e43fdb0a7ed246fca8a808
vessper <none@none>
2011-09-07 15:14:11 +0000

    Core: Update AdvTreeSorter class to expand functionality

--- EveHQ.Core/AdvTreeSorter.vb
+++ EveHQ.Core/AdvTreeSorter.vb
@@ -24,68 +24,92 @@ Public Class AdvTreeSorter
 
     Shared SortByTag As Boolean = False
 
-    Public Shared Sub Sort(ByVal HostedTree As AdvTree, ByVal ColumnDisplayIndex As Integer, ByVal SortChildNodes As Boolean, ByVal RetainLastSortOrder As Boolean)
-        Dim ColIdx As Integer = ColumnDisplayIndex - 1
-        Dim SortOrder As Integer = CInt(HostedTree.Tag)
-        If RetainLastSortOrder = True And SortOrder <> 0 Then
-            ColumnDisplayIndex = Math.Abs(SortOrder)
-            ColIdx = ColumnDisplayIndex - 1
-            SortOrder = -SortOrder
+    Public Shared Function Sort(ByVal Column As DevComponents.AdvTree.ColumnHeader, ByVal SortChildNodes As Boolean, ByVal RetainLastSortOrder As Boolean) As AdvTreeSortResult
+        Dim HostedTree As AdvTree = Column.AdvTree
+        Dim ColumnDisplayIndex As Integer = Column.DisplayIndex
+        Return Sort(HostedTree, ColumnDisplayIndex, SortChildNodes, RetainLastSortOrder)
+    End Function
+
+    Public Shared Function Sort(ByVal HostedTree As AdvTree, ByVal SortResult As AdvTreeSortResult, ByVal SortChildNodes As Boolean) As AdvTreeSortResult
+        Return Sort(HostedTree, SortResult.SortedIndex, SortResult.SortedOrder, SortChildNodes, False)
+    End Function
+
+    Public Shared Function Sort(ByVal HostedTree As AdvTree, ByVal ColumnDisplayIndex As Integer, ByVal SortChildNodes As Boolean, ByVal RetainLastSortOrder As Boolean) As AdvTreeSortResult
+        Return Sort(HostedTree, ColumnDisplayIndex, AdvTreeSortOrder.Default, SortChildNodes, RetainLastSortOrder)
+    End Function
+
+    Private Shared Function Sort(ByVal HostedTree As AdvTree, ByVal ColumnDisplayIndex As Integer, ByVal SortOrder As AdvTreeSortOrder, ByVal SortChildNodes As Boolean, ByVal RetainLastSortOrder As Boolean) As AdvTreeSortResult
+
+        ' Determine sorting logic based on method parameters
+        Dim LastSortResult As AdvTreeSortResult = CType(HostedTree.Tag, AdvTreeSortResult)
+        Dim IndexToSort As Integer = ColumnDisplayIndex
+        Dim OrderToSort As AdvTreeSortOrder = SortOrder
+        If SortOrder = AdvTreeSortOrder.Default Then
+            OrderToSort = AdvTreeSortOrder.Ascending
         End If
-        Dim SortedOrder As SortOrder
+
+        If LastSortResult IsNot Nothing Then
+            If RetainLastSortOrder = True Then
+                IndexToSort = LastSortResult.SortedIndex
+                OrderToSort = LastSortResult.SortedOrder
+            Else
+                If SortOrder = AdvTreeSortOrder.Default Then
+                    If IndexToSort = LastSortResult.SortedIndex Then
+                        OrderToSort = CType(-LastSortResult.SortedOrder, AdvTreeSortOrder)
+                    Else
+                        OrderToSort = CType(LastSortResult.SortedOrder, AdvTreeSortOrder)
+                    End If
+                Else
+                    OrderToSort = SortOrder
+                End If
+            End If
+        End If
+
+        ' Set the "true" column index
+        Dim ColIdx As Integer = IndexToSort - 1
+
+        ' Begin UI update
         HostedTree.Cursor = Cursors.WaitCursor
         HostedTree.BeginUpdate()
-        HostedTree.Columns(ColIdx).ImageAlignment = eColumnImageAlignment.Right
+
+        ' Check if we are to look at the tag instead of text
         If HostedTree.Columns(ColIdx).EditorType = eCellEditorType.Custom Then
             SortByTag = True
         Else
             SortByTag = False
         End If
-        If ColumnDisplayIndex = Math.Abs(SortOrder) Then
-            ' Invert the sort
-            If SortOrder < 0 Then
-                SortedOrder = System.Windows.Forms.SortOrder.Ascending
-                HostedTree.Tag = ColumnDisplayIndex
-                HostedTree.Columns(ColIdx).Image = My.Resources.ArrowUp
-            Else
-                SortedOrder = System.Windows.Forms.SortOrder.Descending
-                HostedTree.Tag = -ColumnDisplayIndex
-                HostedTree.Columns(ColIdx).Image = My.Resources.ArrowDown
-            End If
+
+        ' Reset the old image
+        If LastSortResult IsNot Nothing Then
+            HostedTree.Columns(LastSortResult.SortedIndex - 1).Image = Nothing
+        End If
+
+        ' Set new image
+        HostedTree.Columns(ColIdx).ImageAlignment = eColumnImageAlignment.Right
+        If OrderToSort = AdvTreeSortOrder.Ascending Then
+            HostedTree.Columns(ColIdx).Image = My.Resources.ArrowUp
         Else
-            If SortOrder <> 0 Then
-                HostedTree.Columns(Math.Abs(SortOrder) - 1).Image = Nothing
-            End If
-            If SortOrder < 0 Then
-                SortedOrder = System.Windows.Forms.SortOrder.Descending
-                HostedTree.Tag = -ColumnDisplayIndex
-                HostedTree.Columns(ColIdx).Image = My.Resources.ArrowDown
-            Else
-                SortedOrder = System.Windows.Forms.SortOrder.Ascending
-                HostedTree.Tag = ColumnDisplayIndex
-                HostedTree.Columns(ColIdx).Image = My.Resources.ArrowUp
-            End If
+            HostedTree.Columns(ColIdx).Image = My.Resources.ArrowDown
         End If
-        HostedTree.Nodes.Sort(New AdvTreeSortComparer(ColIdx, SortedOrder, SortByTag))
+
+        HostedTree.Nodes.Sort(New AdvTreeSortComparer(ColIdx, OrderToSort, SortByTag))
         If SortChildNodes = True Then
-            Call PerformIterativeSort(HostedTree.Nodes, ColIdx, SortedOrder)
+            Call PerformIterativeSort(HostedTree.Nodes, ColIdx, OrderToSort)
         End If
+
+        ' End the update
         HostedTree.EndUpdate()
         HostedTree.Cursor = Cursors.Default
-    End Sub
 
-    Public Shared Sub Sort(ByVal Column As DevComponents.AdvTree.ColumnHeader, ByVal SortChildNodes As Boolean, ByVal RetainLastSortOrder As Boolean)
-        Dim HostedTree As AdvTree = Column.AdvTree
-        Dim ColumnDisplayIndex As Integer = Column.DisplayIndex
-        If Column.EditorType = eCellEditorType.Custom Then
-            SortByTag = True
-        Else
-            SortByTag = False
-        End If
-        Call Sort(HostedTree, ColumnDisplayIndex, SortChildNodes, RetainLastSortOrder)
-    End Sub
+        ' Return and store the result
+        Dim NewSortResult As New AdvTreeSortResult(IndexToSort, OrderToSort)
+        HostedTree.Tag = NewSortResult
+        Return NewSortResult
 
-    Private Shared Sub PerformIterativeSort(ByVal SortNodeCollection As NodeCollection, ByVal ColIdx As Integer, ByVal Order As SortOrder)
+    End Function
+
+
+    Private Shared Sub PerformIterativeSort(ByVal SortNodeCollection As NodeCollection, ByVal ColIdx As Integer, ByVal Order As AdvTreeSortOrder)
         For Each SortNode As Node In SortNodeCollection
             If SortNode.Nodes.Count > 0 Then
                 ' Do a sort on these
@@ -101,18 +125,18 @@ End Class
 Public Class AdvTreeSortComparer
     Implements IComparer
     Private col As Integer
-    Private order As SortOrder
+    Private order As AdvTreeSortOrder
     Private sorttag As Boolean = False
     Dim DoubleX, DoubleY As Double
     Dim DateX, DateY As Date
 
     Public Sub New()
         col = 0
-        order = SortOrder.Ascending
+        order = AdvTreeSortOrder.Ascending
         sorttag = False
     End Sub
 
-    Public Sub New(ByVal column As Integer, ByVal order As SortOrder, ByVal SortTag As Boolean)
+    Public Sub New(ByVal column As Integer, ByVal order As AdvTreeSortOrder, ByVal SortTag As Boolean)
         Me.col = column
         Me.order = order
         Me.sorttag = SortTag
@@ -158,7 +182,7 @@ Public Class AdvTreeSortComparer
         End If
 
         ' Determine whether the sort order is descending.
-        If order = SortOrder.Descending Then
+        If order = AdvTreeSortOrder.Descending Then
             ' Invert the value returned by String.Compare.
             returnVal *= -1
         End If
@@ -166,3 +190,24 @@ Public Class AdvTreeSortComparer
     End Function
 
 End Class
+
+<Serializable()> Public Class AdvTreeSortResult
+    Public SortedIndex As Integer
+    Public SortedOrder As AdvTreeSortOrder
+
+    Public Sub New()
+        SortedIndex = 1
+        SortedOrder = AdvTreeSortOrder.Default
+    End Sub
+
+    Public Sub New(ByVal ColumnIndex As Integer, ByVal SortOrder As AdvTreeSortOrder)
+        SortedIndex = ColumnIndex
+        SortedOrder = SortOrder
+    End Sub
+End Class
+
+Public Enum AdvTreeSortOrder
+    Ascending = -1
+    [Default] = 0
+    Descending = 1
+End Enum
