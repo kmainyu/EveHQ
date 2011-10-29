1a7a5b35a1b4c17d848ed78e97f63f7df6f653c5
vessper <none@none>
2011-10-29 15:58:20 +0000

    Core: Update versions for testing

--- EveHQ/My Project/AssemblyInfo.template.vb
+++ EveHQ/My Project/AssemblyInfo.template.vb
@@ -51,6 +51,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.2.2.$WCREV$")> 
-<Assembly: AssemblyFileVersion("2.2.2.$WCREV$")> 
+<Assembly: AssemblyVersion("2.2.3.$WCREV$")> 
+<Assembly: AssemblyFileVersion("2.2.3.$WCREV$")> 
 <Assembly: AssemblyTrademark("$WCDATE=%d/%m/%Y$ (Version 2.2.2)")> 
