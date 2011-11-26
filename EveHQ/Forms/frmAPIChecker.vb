f4439980a916361e208b68df4c31ed258d48bf72
Vessper <none@none>
2011-11-26 11:54:20 +0000

    Core: Add API mappings in API Checker

--- EveHQ/Forms/frmAPIChecker.vb
+++ EveHQ/Forms/frmAPIChecker.vb
@@ -67,6 +67,7 @@ Public Class frmAPIChecker
                 EveAPI.APITypes.MapKills, _
                 EveAPI.APITypes.Conquerables, _
                 EveAPI.APITypes.ErrorList, _
+                EveAPI.APITypes.FWStats, _
                 EveAPI.APITypes.FWTop100, _
                 EveAPI.APITypes.FWMap, _
                 EveAPI.APITypes.ServerStatus, _
@@ -120,12 +121,14 @@ Public Class frmAPIChecker
               EveAPI.APITypes.MedalsAvailable, _
               EveAPI.APITypes.MailMessages, _
               EveAPI.APITypes.Notifications, _
-               EveAPI.APITypes.MailingLists, _
-               EveAPI.APITypes.Research, _
-               EveAPI.APITypes.CharacterInfo, _
-               EveAPI.APITypes.ContactListChar, _
-               EveAPI.APITypes.ContactListCorp, _
-               EveAPI.APITypes.MemberMedals
+              EveAPI.APITypes.MailingLists, _
+              EveAPI.APITypes.Research, _
+              EveAPI.APITypes.CharacterInfo, _
+              EveAPI.APITypes.ContactListChar, _
+              EveAPI.APITypes.ContactListCorp, _
+              EveAPI.APITypes.ContactNotifications, _
+              EveAPI.APITypes.UpcomingCalendarEvents, _
+              EveAPI.APITypes.MemberMedals
                 lblCharacter.Enabled = True : cboCharacter.Enabled = True
                 lblAccount.Enabled = False : cboAccount.Enabled = False
                 lblOtherInfo.Enabled = False : txtOtherInfo.Enabled = False
@@ -145,11 +148,11 @@ Public Class frmAPIChecker
                 lblOtherInfo.Text = "Before RefID:"
                 APIStyle = 6
 
-			Case EveAPI.APITypes.KillLogChar, EveAPI.APITypes.KillLogChar, EveAPI.APITypes.MailBodies
-				lblCharacter.Enabled = True : cboCharacter.Enabled = True
-				lblAccount.Enabled = False : cboAccount.Enabled = False
-				lblOtherInfo.Enabled = True : txtOtherInfo.Enabled = True
-				lblOtherInfo.Text = "IDs:"
+            Case EveAPI.APITypes.KillLogChar, EveAPI.APITypes.KillLogChar, EveAPI.APITypes.MailBodies, EveAPI.APITypes.CalendarEventAttendeesChar
+                lblCharacter.Enabled = True : cboCharacter.Enabled = True
+                lblAccount.Enabled = False : cboAccount.Enabled = False
+                lblOtherInfo.Enabled = True : txtOtherInfo.Enabled = True
+                lblOtherInfo.Text = "IDs:"
                 APIStyle = 7
 
             Case EveAPI.APITypes.WalletJournalChar, EveAPI.APITypes.WalletJournalCorp
@@ -222,4 +225,108 @@ Public Class frmAPIChecker
             MessageBox.Show("There was an error trying to display the requested API. The error was: " & ControlChars.CrLf & ex.Message, "Error Requesting API", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
+
+    ''' <summary>
+    ''' A list of all available APIs for characters together with official access masks represented as the log of the actual mask
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Enum CharacterAPIs As Integer
+        AccountBalances = EveHQ.EveAPI.APITypes.AccountBalancesChar
+        AssetList = EveHQ.EveAPI.APITypes.AssetsChar
+        CalendarEventAttendees = EveHQ.EveAPI.APITypes.CalendarEventAttendeesChar
+        CharacterSheet = EveHQ.EveAPI.APITypes.CharacterSheet
+        ContactList = EveHQ.EveAPI.APITypes.ContactListChar
+        ContactNotifications = EveHQ.EveAPI.APITypes.ContactNotifications
+        FacWarStats = EveHQ.EveAPI.APITypes.FWStatsChar
+        IndustryJobs = EveHQ.EveAPI.APITypes.IndustryChar
+        KillLog = EveHQ.EveAPI.APITypes.KillLogChar
+        MailBodies = EveHQ.EveAPI.APITypes.MailBodies
+        MailingLists = EveHQ.EveAPI.APITypes.MailingLists
+        MailMessages = EveHQ.EveAPI.APITypes.MailMessages
+        MarketOrders = EveHQ.EveAPI.APITypes.OrdersChar
+        Medals = EveHQ.EveAPI.APITypes.MedalsReceived
+        Notifications = EveHQ.EveAPI.APITypes.Notifications
+        NotificationText = EveHQ.EveAPI.APITypes.NotificationTexts
+        Research = EveHQ.EveAPI.APITypes.Research
+        SkillInTraining = EveHQ.EveAPI.APITypes.SkillTraining
+        SkillQueue = EveHQ.EveAPI.APITypes.SkillQueue
+        Standings = EveHQ.EveAPI.APITypes.StandingsChar
+        UpcomingCalendarEvents = EveHQ.EveAPI.APITypes.UpcomingCalendarEvents
+        WalletJournal = EveHQ.EveAPI.APITypes.WalletJournalChar
+        WalletTransactions = EveHQ.EveAPI.APITypes.WalletTransChar
+        CharacterInfoPrivate = EveHQ.EveAPI.APITypes.CharacterInfo
+        CharacterInfoPublic = EveHQ.EveAPI.APITypes.CharacterInfo
+        AccountStatus = EveHQ.EveAPI.APITypes.AccountStatus
+        Contracts = EveHQ.EveAPI.APITypes.ContractsChar
+        ContractItems = EveHQ.EveAPI.APITypes.ContractItemsChar
+        ContractBids = EveHQ.EveAPI.APITypes.ContractBidsChar
+    End Enum
+
+    ''' <summary>
+    ''' A list of all available APIs for corporations together with official access masks represented as the log of the actual mask 
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Enum CorporateAPIs As Integer
+        AccountBalances = EveHQ.EveAPI.APITypes.AccountBalancesCorp
+        AssetList = EveHQ.EveAPI.APITypes.AssetsCorp
+        MemberMedals = EveHQ.EveAPI.APITypes.MemberMedals
+        CorporationSheet = EveHQ.EveAPI.APITypes.CorpSheet
+        ContactList = EveHQ.EveAPI.APITypes.ContactListCorp
+        ContainerLog = EveHQ.EveAPI.APITypes.CorpContainerLog
+        FacWarStats = EveHQ.EveAPI.APITypes.FWStatsCorp
+        IndustryJobs = EveHQ.EveAPI.APITypes.IndustryCorp
+        KillLog = EveHQ.EveAPI.APITypes.KillLogCorp
+        MemberSecurity = EveHQ.EveAPI.APITypes.CorpMemberSecurity
+        MemberSecurityLog = EveHQ.EveAPI.APITypes.CorpMemberSecurityLog
+        MemberTracking = EveHQ.EveAPI.APITypes.CorpMemberTracking
+        MarketOrders = EveHQ.EveAPI.APITypes.OrdersCorp
+        Medals = EveHQ.EveAPI.APITypes.MedalsAvailable
+        OutpostList = EveHQ.EveAPI.APITypes.OutpostList
+        OutpostServiceList = EveHQ.EveAPI.APITypes.OutpostServiceDetail
+        Shareholders = EveHQ.EveAPI.APITypes.CorpShareholders
+        StarbaseDetail = EveHQ.EveAPI.APITypes.POSDetails
+        Standings = EveHQ.EveAPI.APITypes.StandingsCorp
+        StarbaseList = EveHQ.EveAPI.APITypes.POSList
+        WalletJournal = EveHQ.EveAPI.APITypes.WalletJournalCorp
+        WalletTransactions = EveHQ.EveAPI.APITypes.WalletTransCorp
+        Titles = EveHQ.EveAPI.APITypes.CorpTitles
+        Contracts = EveHQ.EveAPI.APITypes.ContractsCorp
+        ContractItems = EveHQ.EveAPI.APITypes.ContractItemsCorp
+        ContractBids = EveHQ.EveAPI.APITypes.ContractBidsCorp
+    End Enum
+
+    ''' <summary>
+    ''' A list of all available APIs for an account (that doesn't require an access mask or additional info)
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Enum AccountAPIs As Integer
+        APIKeyInfo = EveHQ.EveAPI.APITypes.APIKeyInfo
+        Characters = EveHQ.EveAPI.APITypes.Characters
+    End Enum
+
+    ''' <summary>
+    ''' A list of the "static" APIs available
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Enum StaticAPIs As Integer
+        AllianceList = EveHQ.EveAPI.APITypes.AllianceList
+        CertificateTree = EveHQ.EveAPI.APITypes.CertificateTree
+        CharacterID = EveHQ.EveAPI.APITypes.NameToID
+        CharacterInfo = EveHQ.EveAPI.APITypes.CharacterInfo
+        CharacterName = EveHQ.EveAPI.APITypes.IDToName
+        ConquerableStationList = EveHQ.EveAPI.APITypes.Conquerables
+        ErrorList = EveHQ.EveAPI.APITypes.ErrorList
+        FacWarStats = EveHQ.EveAPI.APITypes.FWStats
+        FacWarTopStats = EveHQ.EveAPI.APITypes.FWTop100
+        RefTypes = EveHQ.EveAPI.APITypes.RefTypes
+        SkillTree = EveHQ.EveAPI.APITypes.SkillTree
+        FacWarSystems = EveHQ.EveAPI.APITypes.FWMap
+        Jumps = EveHQ.EveAPI.APITypes.MapJumps
+        Kills = EveHQ.EveAPI.APITypes.MapKills
+        Sovereignty = EveHQ.EveAPI.APITypes.Sovereignty
+        SovereigntyStatus = EveHQ.EveAPI.APITypes.SovereigntyStatus
+        ServerStatus = EveHQ.EveAPI.APITypes.ServerStatus
+        CallList = EveHQ.EveAPI.APITypes.CallList
+    End Enum
+
 End Class
