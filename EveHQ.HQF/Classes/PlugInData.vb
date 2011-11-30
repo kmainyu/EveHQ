20829211210b9a801f4185702b18133807a4356f
Vessper <none@none>
2011-11-30 22:26:20 +0000

    HQF: Updated cache refresh version

--- EveHQ.HQF/Classes/PlugInData.vb
+++ EveHQ.HQF/Classes/PlugInData.vb
@@ -32,7 +32,7 @@ Public Class PlugInData
     Shared moduleAttributeData As DataSet
     Shared UseSerializableData As Boolean = False
     Public Shared ModuleChanges As New SortedList(Of String, String)
-    Shared LastCacheRefresh As String = "2.4.0.3674"
+    Shared LastCacheRefresh As String = "2.4.0.3678"
 
 #Region "Plug-in Interface Properties and Functions"
 
