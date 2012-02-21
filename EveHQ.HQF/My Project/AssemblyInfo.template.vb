676c4011ee9f90043556ae3e58c3c31d076200a2
Vessper <none@none>
2012-02-21 19:19:07 +0000

    Core: Update version numbers

--- EveHQ.HQF/My Project/AssemblyInfo.template.vb
+++ EveHQ.HQF/My Project/AssemblyInfo.template.vb
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.5.0.$REVNUM$")> 
-<Assembly: AssemblyFileVersion("2.5.0.$REVNUM$")> 
+<Assembly: AssemblyVersion("2.5.1.$REVNUM$")> 
+<Assembly: AssemblyFileVersion("2.5.1.$REVNUM$")> 
