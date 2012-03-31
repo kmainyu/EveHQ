19c53aed01adeafac82264f38f82c604e6f95ba4
Vessper <none@none>
2012-03-31 14:09:45 +0100

    Core: Update cache versions, XML and setup

--- EveHQ.Core/DataFunctions.vb
+++ EveHQ.Core/DataFunctions.vb
@@ -35,7 +35,7 @@ Public Class DataFunctions
     Shared IndustryTimeFormat As String = "yyyy-MM-dd HH:mm:ss"
     Shared SQLTimeFormat As String = "yyyyMMdd HH:mm:ss"
     Shared culture As System.Globalization.CultureInfo = New System.Globalization.CultureInfo("en-GB")
-    Shared LastCacheRefresh As String = "2.5.1.3790"
+    Shared LastCacheRefresh As String = "2.6.0.3800"
 
     Public Shared Function CreateEveHQDataDB() As Boolean
         Select Case EveHQ.Core.HQ.EveHQSettings.DBFormat
