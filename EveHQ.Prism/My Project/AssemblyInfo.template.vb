7706e4bdcac12a68faa3bccd459e7284bf67a1cb
Vessper <none@none>
2012-06-27 20:01:17 +0100

    Core: Update version numbers

--- EveHQ.Prism/My Project/AssemblyInfo.template.vb
+++ EveHQ.Prism/My Project/AssemblyInfo.template.vb
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.8.3.$REVNUM$")> 
-<Assembly: AssemblyFileVersion("2.8.3.$REVNUM$")> 
+<Assembly: AssemblyVersion("2.9.0.$REVNUM$")> 
+<Assembly: AssemblyFileVersion("2.9.0.$REVNUM$")> 
