5ea77440cf529bc4bced51928c42e2777436bf04
Vessper <none@none>
2012-08-21 19:09:43 +0100

    Core: Update cache version and XML file

--- EveHQ.HQF/Classes/PlugInData.vb
+++ EveHQ.HQF/Classes/PlugInData.vb
@@ -32,7 +32,7 @@ Public Class PlugInData
     Shared moduleAttributeData As DataSet
     Shared UseSerializableData As Boolean = False
     Public Shared ModuleChanges As New SortedList(Of String, String)
-    Shared LastCacheRefresh As String = "2.10.0.3927"
+    Shared LastCacheRefresh As String = "2.10.1.3933"
 
 #Region "Plug-in Interface Properties and Functions"
 
