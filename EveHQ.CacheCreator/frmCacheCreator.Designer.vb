6c2112d1757d34ce0036e75d6a7e44cb9f809796
Vessper <none@none>
2013-11-07 11:23:00 +0000

    Core: Fix YAML errors in CacheCreator Core: Rebuild static data files HQF: Get icons working again
    --HG--
    branch : v2.300

--- EveHQ.CacheCreator/frmCacheCreator.Designer.vb
+++ EveHQ.CacheCreator/frmCacheCreator.Designer.vb
@@ -28,33 +28,15 @@ Partial Class FrmCacheCreator
         Me.btnGenerateCache = New System.Windows.Forms.Button()
         Me.btnCheckDB = New System.Windows.Forms.Button()
         Me.lblDB = New System.Windows.Forms.Label()
-        Me.btnGenerateHQFIcons = New System.Windows.Forms.Button()
         Me.btnCheckMarketGroup = New System.Windows.Forms.Button()
         Me.lblInfo = New System.Windows.Forms.Label()
         Me.gbCheckingTools = New System.Windows.Forms.GroupBox()
         Me.gbEveHQCacheGeneration = New System.Windows.Forms.GroupBox()
-        Me.gbEveCacheExtraction = New System.Windows.Forms.GroupBox()
-        Me.gbEveServerType = New System.Windows.Forms.GroupBox()
-        Me.rbBuckingham = New System.Windows.Forms.RadioButton()
-        Me.rbDuality = New System.Windows.Forms.RadioButton()
-        Me.rbSingularity = New System.Windows.Forms.RadioButton()
-        Me.rbTranquility = New System.Windows.Forms.RadioButton()
-        Me.btnSQL = New System.Windows.Forms.Button()
-        Me.lvwLog = New System.Windows.Forms.ListView()
-        Me.colTime = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.colDetails = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.btnXML = New System.Windows.Forms.Button()
-        Me.btnEveLocation = New System.Windows.Forms.Button()
-        Me.lblEveProtocol = New System.Windows.Forms.Label()
-        Me.lblEveProtocolLabel = New System.Windows.Forms.Label()
-        Me.lblEveLocationLabel = New System.Windows.Forms.Label()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.gbInstructions = New System.Windows.Forms.GroupBox()
         Me.lblInstructions = New System.Windows.Forms.Label()
         Me.gbCheckingTools.SuspendLayout()
         Me.gbEveHQCacheGeneration.SuspendLayout()
-        Me.gbEveCacheExtraction.SuspendLayout()
-        Me.gbEveServerType.SuspendLayout()
         Me.gbInstructions.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -93,15 +75,6 @@ Partial Class FrmCacheCreator
         Me.lblDB.TabIndex = 7
         Me.lblDB.Text = "Database:"
         '
-        'btnGenerateHQFIcons
-        '
-        Me.btnGenerateHQFIcons.Location = New System.Drawing.Point(9, 147)
-        Me.btnGenerateHQFIcons.Name = "btnGenerateHQFIcons"
-        Me.btnGenerateHQFIcons.Size = New System.Drawing.Size(299, 23)
-        Me.btnGenerateHQFIcons.TabIndex = 8
-        Me.btnGenerateHQFIcons.Text = "Generate HQF Icons"
-        Me.btnGenerateHQFIcons.UseVisualStyleBackColor = True
-        '
         'btnCheckMarketGroup
         '
         Me.btnCheckMarketGroup.Location = New System.Drawing.Point(6, 19)
@@ -123,9 +96,9 @@ Partial Class FrmCacheCreator
         'gbCheckingTools
         '
         Me.gbCheckingTools.Controls.Add(Me.btnCheckMarketGroup)
-        Me.gbCheckingTools.Location = New System.Drawing.Point(9, 176)
+        Me.gbCheckingTools.Location = New System.Drawing.Point(9, 147)
         Me.gbCheckingTools.Name = "gbCheckingTools"
-        Me.gbCheckingTools.Size = New System.Drawing.Size(299, 113)
+        Me.gbCheckingTools.Size = New System.Drawing.Size(299, 58)
         Me.gbCheckingTools.TabIndex = 11
         Me.gbCheckingTools.TabStop = False
         Me.gbCheckingTools.Text = "Checking Tools"
@@ -136,182 +109,32 @@ Partial Class FrmCacheCreator
         Me.gbEveHQCacheGeneration.Controls.Add(Me.gbCheckingTools)
         Me.gbEveHQCacheGeneration.Controls.Add(Me.btnGenerateCache)
         Me.gbEveHQCacheGeneration.Controls.Add(Me.btnCheckDB)
-        Me.gbEveHQCacheGeneration.Controls.Add(Me.btnGenerateHQFIcons)
         Me.gbEveHQCacheGeneration.Controls.Add(Me.lblDB)
         Me.gbEveHQCacheGeneration.Location = New System.Drawing.Point(12, 12)
         Me.gbEveHQCacheGeneration.Name = "gbEveHQCacheGeneration"
-        Me.gbEveHQCacheGeneration.Size = New System.Drawing.Size(318, 302)
+        Me.gbEveHQCacheGeneration.Size = New System.Drawing.Size(318, 213)
         Me.gbEveHQCacheGeneration.TabIndex = 12
         Me.gbEveHQCacheGeneration.TabStop = False
         Me.gbEveHQCacheGeneration.Text = "EveHQ Cache Generation"
-        '
-        'gbEveCacheExtraction
-        '
-        Me.gbEveCacheExtraction.Controls.Add(Me.gbEveServerType)
-        Me.gbEveCacheExtraction.Controls.Add(Me.btnSQL)
-        Me.gbEveCacheExtraction.Controls.Add(Me.lvwLog)
-        Me.gbEveCacheExtraction.Controls.Add(Me.btnXML)
-        Me.gbEveCacheExtraction.Controls.Add(Me.btnEveLocation)
-        Me.gbEveCacheExtraction.Controls.Add(Me.lblEveProtocol)
-        Me.gbEveCacheExtraction.Controls.Add(Me.lblEveProtocolLabel)
-        Me.gbEveCacheExtraction.Controls.Add(Me.lblEveLocationLabel)
-        Me.gbEveCacheExtraction.Location = New System.Drawing.Point(12, 320)
-        Me.gbEveCacheExtraction.Name = "gbEveCacheExtraction"
-        Me.gbEveCacheExtraction.Size = New System.Drawing.Size(787, 530)
-        Me.gbEveCacheExtraction.TabIndex = 13
-        Me.gbEveCacheExtraction.TabStop = False
-        Me.gbEveCacheExtraction.Text = "Eve Cache Extraction"
-        '
-        'gbEveServerType
-        '
-        Me.gbEveServerType.Controls.Add(Me.rbBuckingham)
-        Me.gbEveServerType.Controls.Add(Me.rbDuality)
-        Me.gbEveServerType.Controls.Add(Me.rbSingularity)
-        Me.gbEveServerType.Controls.Add(Me.rbTranquility)
-        Me.gbEveServerType.Location = New System.Drawing.Point(519, 21)
-        Me.gbEveServerType.Name = "gbEveServerType"
-        Me.gbEveServerType.Size = New System.Drawing.Size(262, 75)
-        Me.gbEveServerType.TabIndex = 9
-        Me.gbEveServerType.TabStop = False
-        Me.gbEveServerType.Text = "Eve Server Name"
-        '
-        'rbBuckingham
-        '
-        Me.rbBuckingham.AutoSize = True
-        Me.rbBuckingham.Location = New System.Drawing.Point(111, 44)
-        Me.rbBuckingham.Name = "rbBuckingham"
-        Me.rbBuckingham.Size = New System.Drawing.Size(84, 17)
-        Me.rbBuckingham.TabIndex = 3
-        Me.rbBuckingham.Text = "Buckingham"
-        Me.rbBuckingham.UseVisualStyleBackColor = True
-        '
-        'rbDuality
-        '
-        Me.rbDuality.AutoSize = True
-        Me.rbDuality.Location = New System.Drawing.Point(111, 21)
-        Me.rbDuality.Name = "rbDuality"
-        Me.rbDuality.Size = New System.Drawing.Size(57, 17)
-        Me.rbDuality.TabIndex = 2
-        Me.rbDuality.Text = "Duality"
-        Me.rbDuality.UseVisualStyleBackColor = True
-        '
-        'rbSingularity
-        '
-        Me.rbSingularity.AutoSize = True
-        Me.rbSingularity.Location = New System.Drawing.Point(7, 44)
-        Me.rbSingularity.Name = "rbSingularity"
-        Me.rbSingularity.Size = New System.Drawing.Size(73, 17)
-        Me.rbSingularity.TabIndex = 1
-        Me.rbSingularity.Text = "Singularity"
-        Me.rbSingularity.UseVisualStyleBackColor = True
-        '
-        'rbTranquility
-        '
-        Me.rbTranquility.AutoSize = True
-        Me.rbTranquility.Checked = True
-        Me.rbTranquility.Location = New System.Drawing.Point(7, 21)
-        Me.rbTranquility.Name = "rbTranquility"
-        Me.rbTranquility.Size = New System.Drawing.Size(73, 17)
-        Me.rbTranquility.TabIndex = 0
-        Me.rbTranquility.TabStop = True
-        Me.rbTranquility.Text = "Tranquility"
-        Me.rbTranquility.UseVisualStyleBackColor = True
-        '
-        'btnSQL
-        '
-        Me.btnSQL.Location = New System.Drawing.Point(112, 73)
-        Me.btnSQL.Name = "btnSQL"
-        Me.btnSQL.Size = New System.Drawing.Size(100, 23)
-        Me.btnSQL.TabIndex = 8
-        Me.btnSQL.Text = "Update SQL"
-        Me.btnSQL.UseVisualStyleBackColor = True
-        '
-        'lvwLog
-        '
-        Me.lvwLog.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.lvwLog.AutoArrange = False
-        Me.lvwLog.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.colTime, Me.colDetails})
-        Me.lvwLog.Location = New System.Drawing.Point(6, 102)
-        Me.lvwLog.Name = "lvwLog"
-        Me.lvwLog.Size = New System.Drawing.Size(775, 422)
-        Me.lvwLog.TabIndex = 7
-        Me.lvwLog.UseCompatibleStateImageBehavior = False
-        Me.lvwLog.View = System.Windows.Forms.View.Details
-        '
-        'colTime
-        '
-        Me.colTime.Text = "Time"
-        Me.colTime.Width = 150
-        '
-        'colDetails
-        '
-        Me.colDetails.Text = "Details"
-        Me.colDetails.Width = 600
-        '
-        'btnXML
-        '
-        Me.btnXML.Location = New System.Drawing.Point(6, 73)
-        Me.btnXML.Name = "btnXML"
-        Me.btnXML.Size = New System.Drawing.Size(100, 23)
-        Me.btnXML.TabIndex = 6
-        Me.btnXML.Text = "Dump As XML"
-        Me.btnXML.UseVisualStyleBackColor = True
-        '
-        'btnEveLocation
-        '
-        Me.btnEveLocation.Location = New System.Drawing.Point(88, 21)
-        Me.btnEveLocation.Name = "btnEveLocation"
-        Me.btnEveLocation.Size = New System.Drawing.Size(368, 20)
-        Me.btnEveLocation.TabIndex = 5
-        Me.btnEveLocation.Text = "<unknown>"
-        Me.btnEveLocation.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.ToolTip1.SetToolTip(Me.btnEveLocation, "Click to change the location of the Eve executable")
-        Me.btnEveLocation.UseVisualStyleBackColor = True
-        '
-        'lblEveProtocol
-        '
-        Me.lblEveProtocol.AutoSize = True
-        Me.lblEveProtocol.Location = New System.Drawing.Point(85, 47)
-        Me.lblEveProtocol.Name = "lblEveProtocol"
-        Me.lblEveProtocol.Size = New System.Drawing.Size(63, 13)
-        Me.lblEveProtocol.TabIndex = 3
-        Me.lblEveProtocol.Text = "<unknown>"
-        '
-        'lblEveProtocolLabel
-        '
-        Me.lblEveProtocolLabel.AutoSize = True
-        Me.lblEveProtocolLabel.Location = New System.Drawing.Point(6, 47)
-        Me.lblEveProtocolLabel.Name = "lblEveProtocolLabel"
-        Me.lblEveProtocolLabel.Size = New System.Drawing.Size(71, 13)
-        Me.lblEveProtocolLabel.TabIndex = 2
-        Me.lblEveProtocolLabel.Text = "Eve Protocol:"
-        '
-        'lblEveLocationLabel
-        '
-        Me.lblEveLocationLabel.AutoSize = True
-        Me.lblEveLocationLabel.Location = New System.Drawing.Point(6, 25)
-        Me.lblEveLocationLabel.Name = "lblEveLocationLabel"
-        Me.lblEveLocationLabel.Size = New System.Drawing.Size(73, 13)
-        Me.lblEveLocationLabel.TabIndex = 0
-        Me.lblEveLocationLabel.Text = "Eve Location:"
         '
         'gbInstructions
         '
         Me.gbInstructions.Controls.Add(Me.lblInstructions)
         Me.gbInstructions.Location = New System.Drawing.Point(336, 12)
         Me.gbInstructions.Name = "gbInstructions"
-        Me.gbInstructions.Size = New System.Drawing.Size(463, 302)
+        Me.gbInstructions.Size = New System.Drawing.Size(257, 213)
         Me.gbInstructions.TabIndex = 14
         Me.gbInstructions.TabStop = False
         Me.gbInstructions.Text = "Instructions!"
         '
         'lblInstructions
         '
+        Me.lblInstructions.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lblInstructions.Location = New System.Drawing.Point(7, 20)
         Me.lblInstructions.Name = "lblInstructions"
-        Me.lblInstructions.Size = New System.Drawing.Size(450, 279)
+        Me.lblInstructions.Size = New System.Drawing.Size(244, 185)
         Me.lblInstructions.TabIndex = 0
         Me.lblInstructions.Text = resources.GetString("lblInstructions.Text")
         '
@@ -319,20 +142,16 @@ Partial Class FrmCacheCreator
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(811, 862)
+        Me.ClientSize = New System.Drawing.Size(600, 232)
         Me.Controls.Add(Me.gbInstructions)
-        Me.Controls.Add(Me.gbEveCacheExtraction)
         Me.Controls.Add(Me.gbEveHQCacheGeneration)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "FrmCacheCreator"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "EveHQ Cache Creator"
         Me.gbCheckingTools.ResumeLayout(False)
         Me.gbEveHQCacheGeneration.ResumeLayout(False)
         Me.gbEveHQCacheGeneration.PerformLayout()
-        Me.gbEveCacheExtraction.ResumeLayout(False)
-        Me.gbEveCacheExtraction.PerformLayout()
-        Me.gbEveServerType.ResumeLayout(False)
-        Me.gbEveServerType.PerformLayout()
         Me.gbInstructions.ResumeLayout(False)
         Me.ResumeLayout(False)
 
@@ -341,28 +160,12 @@ Partial Class FrmCacheCreator
     Friend WithEvents btnGenerateCache As System.Windows.Forms.Button
     Friend WithEvents btnCheckDB As System.Windows.Forms.Button
     Friend WithEvents lblDB As System.Windows.Forms.Label
-    Friend WithEvents btnGenerateHQFIcons As System.Windows.Forms.Button
     Friend WithEvents btnCheckMarketGroup As System.Windows.Forms.Button
     Friend WithEvents lblInfo As System.Windows.Forms.Label
     Friend WithEvents gbCheckingTools As System.Windows.Forms.GroupBox
     Friend WithEvents gbEveHQCacheGeneration As System.Windows.Forms.GroupBox
-    Friend WithEvents gbEveCacheExtraction As System.Windows.Forms.GroupBox
-    Friend WithEvents lblEveLocationLabel As System.Windows.Forms.Label
-    Friend WithEvents lblEveProtocol As System.Windows.Forms.Label
-    Friend WithEvents lblEveProtocolLabel As System.Windows.Forms.Label
-    Friend WithEvents btnEveLocation As System.Windows.Forms.Button
     Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
-    Friend WithEvents btnXML As System.Windows.Forms.Button
-    Friend WithEvents lvwLog As System.Windows.Forms.ListView
-    Friend WithEvents colTime As System.Windows.Forms.ColumnHeader
-    Friend WithEvents colDetails As System.Windows.Forms.ColumnHeader
     Friend WithEvents gbInstructions As System.Windows.Forms.GroupBox
     Friend WithEvents lblInstructions As System.Windows.Forms.Label
-    Friend WithEvents btnSQL As System.Windows.Forms.Button
-    Friend WithEvents gbEveServerType As System.Windows.Forms.GroupBox
-    Friend WithEvents rbDuality As System.Windows.Forms.RadioButton
-    Friend WithEvents rbSingularity As System.Windows.Forms.RadioButton
-    Friend WithEvents rbTranquility As System.Windows.Forms.RadioButton
-    Friend WithEvents rbBuckingham As System.Windows.Forms.RadioButton
 
 End Class
