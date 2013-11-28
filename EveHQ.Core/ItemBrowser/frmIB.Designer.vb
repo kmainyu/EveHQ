51c995273a3bd7dca32bc787545698ecfb17bc74
Arkan5 <el_pollo_del_muerto@gmx.net>
2013-11-28 15:34:55 +0100

    ItemBrowser: Removed unnecessary RequiredSkills resource

--- EveHQ.Core/ItemBrowser/frmIB.Designer.vb
+++ EveHQ.Core/ItemBrowser/frmIB.Designer.vb
@@ -1498,8 +1498,7 @@
             Me.adtSkills.ItemUsableTime = CType(0, Long)
             Me.adtSkills.Location = New System.Drawing.Point(1, 1)
             Me.adtSkills.Name = "adtSkills"
-            Dim temp = CType(resources.GetObject("adtSkills.RequiredSkills"), System.Collections.Generic.SortedList(Of String, Integer))
-            Me.adtSkills.RequiredSkills = temp.ToDictionary(Of String, Integer)(Function(pair) pair.Key, Function(pair) pair.Value)
+            Me.adtSkills.RequiredSkills = New Dictionary(Of String, Integer)()
             Me.adtSkills.Size = New System.Drawing.Size(699, 590)
             Me.adtSkills.TabIndex = 0
             '
