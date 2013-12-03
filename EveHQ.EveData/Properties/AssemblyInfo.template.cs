1f8c14f4b92efa89c91f707f0ce5eacc004d04f6
Arkan5 <el_pollo_del_muerto@gmx.net>
2013-12-03 12:36:54 +0100

    Prism: Added total value back to Transaction Trading Report

--- EveHQ.EveData/Properties/AssemblyInfo.template.cs
+++ EveHQ.EveData/Properties/AssemblyInfo.template.cs
@@ -17,10 +17,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Software Addicts Studios")]
 [assembly: AssemblyProduct("EveHQ")]
 [assembly: AssemblyCopyright("� EveHQ Development Team 2005-2013")]
-[assembly: AssemblyFileVersion("2.13.3.$REVNUM$-Beta-")]
-[assembly: AssemblyVersion("2.13.3.$REVNUM$")]
+[assembly: AssemblyFileVersion("2.13.4.$REVNUM$-Beta-")]
+[assembly: AssemblyVersion("2.13.4.$REVNUM$")]
 [assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyTrademark("$UTCDATE$ (Version 2.13.3.$REVNUM$-Beta-)")]
+[assembly: AssemblyTrademark("$UTCDATE$ (Version 2.13.4.$REVNUM$-Beta-)")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
