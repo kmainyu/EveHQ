db121bdf8b06843b741de020818b9651fcc8a168
quantix <quantix.blackstar@gmail.com>
2014-06-01 14:14:45 -0700

    Core: Fixed the name order for market regions and systems in the settings panel.

--- EveHQ/Forms/frmSettings.vb
+++ EveHQ/Forms/frmSettings.vb
@@ -2283,7 +2283,9 @@ Namespace Forms
                 End If
 
                 If (HQ.MarketStatDataProvider.LimitedRegionSelection = False) Then
-                    For Each regionName As String In StaticData.Regions.Values
+                    For Each regionName As String In StaticData.Regions.Values.OrderBy(Function(r As String) As String
+                                                                                           Return r
+                                                                                       End Function)
                         _regionList.Items.Add(regionName)
                     Next
                 Else
@@ -2295,13 +2297,16 @@ Namespace Forms
                 End If
             End If
 
+
             If _systemList IsNot Nothing Then
                 If _systemList.Items IsNot Nothing Then
                     _systemList.Items.Clear()
                 End If
 
                 If (HQ.MarketStatDataProvider.LimitedSystemSelection = False) Then
-                    For Each system As SolarSystem In StaticData.SolarSystems.Values
+                    For Each system As SolarSystem In StaticData.SolarSystems.Values.OrderBy(Function(r As SolarSystem) As String
+                                                                                                 Return r.Name
+                                                                                             End Function)
                         _systemList.Items.Add(system.Name)
                     Next
                 Else
